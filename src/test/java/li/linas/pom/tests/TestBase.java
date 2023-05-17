66a748c7fe09f9e127a11b1e20ab43a5c53d1352
LinasKursai <linaskursai@gmail.com>
2023-05-18 01:13:38 +0300

    Add TestBase class

---
+++ src/test/java/li/linas/pom/tests/TestBase.java
@@ -0,0 +1,16 @@
+package li.linas.pom.tests;
+
+import li.linas.pom.pages.Common;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public abstract class TestBase {
+
+    @BeforeMethod
+    public abstract void seTup();
+
+    @AfterMethod
+    public void tearDown() {
+        Common.closeDriver();
+    }
+}
